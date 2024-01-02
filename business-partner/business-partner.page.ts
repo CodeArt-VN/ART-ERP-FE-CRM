abe62626167512f23b59b35473e90b6d3fd346c3
hungvq <hungvq@live.com>
2024-01-02 15:49:13 +0700

    update layout

--- business-partner/business-partner.page.ts
+++ business-partner/business-partner.page.ts
@@ -16,42 +16,47 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 export class BusinessPartnerPage extends PageBase {
     branchList = [];
     statusList = [];
-    filesDuplicate=[];
-    findByValue;
+    findByValue = 'WorkPhone';
     isShowMergePopup = false;
-    formMerge:any
-    mergeModel={
-        Code :'',
-        Name:'',
-        WorkPhone:'',
-        TaxCode:'',
-        Id:0,
-        Address:{
-            AddressLine1:'',
-            AddressLine2:'',
-            Contact:'',
-            Country:'',
-            District:'',
-            Id:'',
-            Phone1:'',
-            Phone2:'',
-            Province:'',
-            Ward:''
+    mergeFunction = {
+        isShowMergePopup: false,
+        showSpinner: false,
+        findBy: 'WorkPhone',
+        items: [],
+    };
+    formMerge: any
+    mergeModel = {
+        Code: '',
+        Name: '',
+        WorkPhone: '',
+        TaxCode: '',
+        Id: 0,
+        Address: {
+            AddressLine1: '',
+            AddressLine2: '',
+            Contact: '',
+            Country: '',
+            District: '',
+            Id: '',
+            Phone1: '',
+            Phone2: '',
+            Province: '',
+            Ward: ''
         },
-        BillingAddress:'',
-        Email:'',
-        CompanyName:'',
-        IsPersonal:false,
-    } ;
+        BillingAddress: '',
+        Email: '',
+        CompanyName: '',
+        IsPersonal: false,
+    };
 
     constructor(
         private renderer: Renderer2,
         public pageProvider: CRM_ContactProvider,
         public branchProvider: BRA_BranchProvider,
         public modalController: ModalController,
-		public popoverCtrl: PopoverController,
+        public popoverCtrl: PopoverController,
         public alertCtrl: AlertController,
-         public loadingController: LoadingController,
+        public loadingController: LoadingController,
         public env: EnvService,
         public formBuilder: FormBuilder,
         public navCtrl: NavController,
@@ -66,7 +71,7 @@ export class BusinessPartnerPage extends PageBase {
             this.sort.Id = 'Id';
             this.sortToggle('Id', true);
         }
-        
+
         if (this.pageConfig.pageName == 'vendor') {
             this.query.IgnoredBranch = true;
             this.query.IsVendor = true;
@@ -92,14 +97,14 @@ export class BusinessPartnerPage extends PageBase {
             this.query.IsCustomer = true;
             this.query.IDOwner = this.pageConfig.canViewAllData ? 'all' : this.env.user.StaffID;
         }
-        else if (this.pageConfig.pageName == 'business-partner' || this.pageConfig.pageName == 'contact-mobile'){
+        else if (this.pageConfig.pageName == 'business-partner' || this.pageConfig.pageName == 'contact-mobile') {
             this.query.IDOwner = this.pageConfig.canViewAllData ? 'all' : this.env.user.StaffID;
         }
 
         Promise.all([
             this.branchProvider.read(),
             this.env.getStatus('BusinessPartner')
-        ]).then((values:any) => {
+        ]).then((values: any) => {
             this.branchList = values[0]['data'];
             this.statusList = values[1];
 
@@ -187,7 +192,7 @@ export class BusinessPartnerPage extends PageBase {
                                     if (publishEventCode) {
                                         this.env.publishEvent({ Code: publishEventCode });
                                     }
-                                    this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete','success');
+                                    this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete', 'success');
                                     this.submitAttempt = false;
 
                                 }).catch(err => {
@@ -207,10 +212,10 @@ export class BusinessPartnerPage extends PageBase {
 
     approveBusinessPartner() {
 
-        
+
         let itemsCanNotProcess = this.selectedItems.filter(i => !(i.Status == "Submitted"));
         if (itemsCanNotProcess.length == this.selectedItems.length) {
-            this.env.showTranslateMessage('erp.app.pages.crm.business-partner.message.approve-business-partner-error','warning');
+            this.env.showTranslateMessage('erp.app.pages.crm.business-partner.message.approve-business-partner-error', 'warning');
         }
         else {
             itemsCanNotProcess.forEach(i => {
@@ -240,25 +245,25 @@ export class BusinessPartnerPage extends PageBase {
                                 method: "POST",
                                 url: function () { return ApiSetting.apiDomain("CRM/Contact/ApproveBusinessPartners/") }
                             };
-                    
+
                             if (this.submitAttempt == false) {
                                 this.submitAttempt = true;
-                    
+
                                 let postDTO = { Ids: [] };
                                 postDTO.Ids = this.selectedItems.map(e => e.Id);
-                    
+
                                 this.pageProvider.commonService.connect(apiPath.method, apiPath.url(), postDTO).toPromise()
                                     .then((savedItem: any) => {
                                         if (publishEventCode) {
                                             this.env.publishEvent({ Code: publishEventCode });
                                         }
-                                        this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete','success');
+                                        this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete', 'success');
                                         this.submitAttempt = false;
-                    
+
                                     }).catch(err => {
                                         this.submitAttempt = false;
                                         //console.log(err);
-                                });
+                                    });
                             }
                         }
                     }
@@ -272,7 +277,7 @@ export class BusinessPartnerPage extends PageBase {
     disapproveBusinessPartner() {
         let itemsCanNotProcess = this.selectedItems.filter(i => !(i.Status == 'Submitted' || i.Status == 'Approved'));
         if (itemsCanNotProcess.length == this.selectedItems.length) {
-            this.env.showTranslateMessage('erp.app.pages.crm.business-partner.message.disapprove-business-partner-error','warning');
+            this.env.showTranslateMessage('erp.app.pages.crm.business-partner.message.disapprove-business-partner-error', 'warning');
         }
         else {
             itemsCanNotProcess.forEach(i => {
@@ -302,25 +307,25 @@ export class BusinessPartnerPage extends PageBase {
                                 method: "POST",
                                 url: function () { return ApiSetting.apiDomain("CRM/Contact/DisapproveBusinessPartners/") }
                             };
-                    
+
                             if (this.submitAttempt == false) {
                                 this.submitAttempt = true;
-                    
+
                                 let postDTO = { Ids: [] };
                                 postDTO.Ids = this.selectedItems.map(e => e.Id);
-                    
+
                                 this.pageProvider.commonService.connect(apiPath.method, apiPath.url(), postDTO).toPromise()
                                     .then((savedItem: any) => {
                                         if (publishEventCode) {
                                             this.env.publishEvent({ Code: publishEventCode });
                                         }
-                                        this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete','success');
+                                        this.env.showTranslateMessage('erp.app.pages.sale.sale-order.message.save-complete', 'success');
                                         this.submitAttempt = false;
-                    
+
                                     }).catch(err => {
                                         this.submitAttempt = false;
                                         //console.log(err);
-                                });
+                                    });
                             }
                         }
                     }
@@ -331,125 +336,130 @@ export class BusinessPartnerPage extends PageBase {
         }
     }
 
-    changeDuplicateKeywork(e){
-        if (e.target.value=='WorkPhone'){
-           this.findByValue = 'WorkPhone';
-           this.query.TaxCode_eq =undefined;
+    changeDuplicateKeywork(e) {
+        if (e.target.value == 'WorkPhone') {
+            this.findByValue = 'WorkPhone';
+            this.query.TaxCode_eq = undefined;
         }
-        else{
+        else {
             this.findByValue = 'TaxCode'
-            this.query.WorkPhone_eq=undefined;
-          }
+            this.query.WorkPhone_eq = undefined;
+        }
         console.log(e);
     }
-    findByDuplicate(){
-        let q = {"FindBy": this.findByValue};
+    findByDuplicate() {
+        let q = { "FindBy": this.findByValue };
         this.query.IgnoredBranch = true;
+        this.mergeFunction.items = [];
+        this.mergeFunction.showSpinner = true;
+
         let apiPath = { method: "GET", url: function () { return ApiSetting.apiDomain("CRM/Contact/FindDuplicates") } };
-        this.pageProvider.commonService.connect(apiPath.method, apiPath.url(),q).toPromise().then((data: any) => {
-        this.filesDuplicate = data;
+        this.pageProvider.commonService.connect(apiPath.method, apiPath.url(), q).toPromise().then((data: any) => {
+            this.mergeFunction.items = data;
+        }).finally(() => {  
+            this.mergeFunction.showSpinner = false;
         })
     }
-    showMergePopup(e){
-        this.isShowMergePopup= true;
+    showMergePopup(e) {
+        this.isShowMergePopup = true;
         this.changeSelected(this.selectedItems[0])
     }
-   
-    changeSelected(selectedModel){
-          this.mergeModel.Code =selectedModel.Code;
-          this.mergeModel.Name= selectedModel.Name;
-          this.mergeModel.WorkPhone= selectedModel.WorkPhone;
-          this.mergeModel.TaxCode= selectedModel.TaxCode;
-          this.mergeModel.BillingAddress= selectedModel.BillingAddress;
-          this.mergeModel.CompanyName= selectedModel.CompanyName;
-          this.mergeModel.Email= selectedModel.Email;
-          this.mergeModel.Id= selectedModel.Id;
-          this.mergeModel.IsPersonal= selectedModel.IsPersonal;
-          
-          this.mergeModel.Address.Id = selectedModel.Addresses[0]?.Id
-          this.mergeModel.Address.AddressLine1 = selectedModel.Addresses[0]?.AddressLine1;
-          this.mergeModel.Address.AddressLine2 = selectedModel.Addresses[0]?.AddressLine2;
-          this.mergeModel.Address.Contact = selectedModel.Addresses[0]?.Contact;
-          this.mergeModel.Address.Country = selectedModel.Addresses[0]?.Country;
-          this.mergeModel.Address.Phone1 = selectedModel.Addresses[0]?.Phone1;
-          this.mergeModel.Address.Phone2 = selectedModel.Addresses[0]?.Phone2;
-          this.mergeModel.Address.Province = selectedModel.Addresses[0]?.Province;
-          this.mergeModel.Address.Ward = selectedModel.Addresses[0]?.Ward;
-          console.log(  this.mergeModel)
-        }
-    mergeContact(){
+
+    changeSelected(selectedModel) {
+        this.mergeModel.Code = selectedModel.Code;
+        this.mergeModel.Name = selectedModel.Name;
+        this.mergeModel.WorkPhone = selectedModel.WorkPhone;
+        this.mergeModel.TaxCode = selectedModel.TaxCode;
+        this.mergeModel.BillingAddress = selectedModel.BillingAddress;
+        this.mergeModel.CompanyName = selectedModel.CompanyName;
+        this.mergeModel.Email = selectedModel.Email;
+        this.mergeModel.Id = selectedModel.Id;
+        this.mergeModel.IsPersonal = selectedModel.IsPersonal;
+
+        this.mergeModel.Address.Id = selectedModel.Addresses[0]?.Id
+        this.mergeModel.Address.AddressLine1 = selectedModel.Addresses[0]?.AddressLine1;
+        this.mergeModel.Address.AddressLine2 = selectedModel.Addresses[0]?.AddressLine2;
+        this.mergeModel.Address.Contact = selectedModel.Addresses[0]?.Contact;
+        this.mergeModel.Address.Country = selectedModel.Addresses[0]?.Country;
+        this.mergeModel.Address.Phone1 = selectedModel.Addresses[0]?.Phone1;
+        this.mergeModel.Address.Phone2 = selectedModel.Addresses[0]?.Phone2;
+        this.mergeModel.Address.Province = selectedModel.Addresses[0]?.Province;
+        this.mergeModel.Address.Ward = selectedModel.Addresses[0]?.Ward;
+        console.log(this.mergeModel)
+    }
+    mergeContact() {
         // so sánh với selected hiện tại 
-        let obj:any={};
-        obj.Id =  this.mergeModel.Id;
-        obj.MergedItems =   this.selectedItems.map(({ Id }) => Id).filter(x=>x!= obj.Id);
-        let selectedMergeModel = this.selectedItems.find(x=>x.Id == obj.Id);
-        let address:any ={};
+        let obj: any = {};
+        obj.Id = this.mergeModel.Id;
+        obj.MergedItems = this.selectedItems.map(({ Id }) => Id).filter(x => x != obj.Id);
+        let selectedMergeModel = this.selectedItems.find(x => x.Id == obj.Id);
+        let address: any = {};
         //obj.QueryStrings={key:''}
-        if(this.mergeModel.Code != selectedMergeModel.Code ){
+        if (this.mergeModel.Code != selectedMergeModel.Code) {
             obj.Code = this.mergeModel.Code;
-        } 
-        if(this.mergeModel.Name != selectedMergeModel.Name ){
+        }
+        if (this.mergeModel.Name != selectedMergeModel.Name) {
             obj.Name = this.mergeModel.Name;
         }
-        if(this.mergeModel.WorkPhone != selectedMergeModel.WorkPhone ){
+        if (this.mergeModel.WorkPhone != selectedMergeModel.WorkPhone) {
             obj.WorkPhone = this.mergeModel.WorkPhone;
-        } 
-        if(this.mergeModel.TaxCode != selectedMergeModel.TaxCode ){
-        obj.TaxCode = this.mergeModel.TaxCode;
-        } 
-        if(this.mergeModel.BillingAddress != selectedMergeModel.BillingAddress ){
+        }
+        if (this.mergeModel.TaxCode != selectedMergeModel.TaxCode) {
+            obj.TaxCode = this.mergeModel.TaxCode;
+        }
+        if (this.mergeModel.BillingAddress != selectedMergeModel.BillingAddress) {
             obj.BillingAddress = this.mergeModel.BillingAddress;
-        }  
-        if(this.mergeModel.Email != selectedMergeModel.Email ){
+        }
+        if (this.mergeModel.Email != selectedMergeModel.Email) {
             obj.Email = this.mergeModel.Email;
-        }  
-        if(this.mergeModel.IsPersonal != selectedMergeModel.IsPersonal ){
+        }
+        if (this.mergeModel.IsPersonal != selectedMergeModel.IsPersonal) {
             obj.IsPersonal = this.mergeModel.IsPersonal;
-        }  
-        if(this.mergeModel.IsPersonal != selectedMergeModel.IsPersonal ){
+        }
+        if (this.mergeModel.IsPersonal != selectedMergeModel.IsPersonal) {
             obj.IsPersonal = this.mergeModel.IsPersonal;
-        }  
-        if(this.mergeModel.Address.AddressLine1 != selectedMergeModel.Addresses[0]?.AddressLine1 ){
+        }
+        if (this.mergeModel.Address.AddressLine1 != selectedMergeModel.Addresses[0]?.AddressLine1) {
             address.AddressLine1 = this.mergeModel.Address.AddressLine1;
-        }  
-        if(this.mergeModel.Address.AddressLine2 != selectedMergeModel.Addresses[0]?.AddressLine2 ){
-            address.AddressLine2= this.mergeModel.Address.AddressLine2;
-        }  
-        if(this.mergeModel.Address.Contact != selectedMergeModel.Addresses[0]?.Contact ){
+        }
+        if (this.mergeModel.Address.AddressLine2 != selectedMergeModel.Addresses[0]?.AddressLine2) {
+            address.AddressLine2 = this.mergeModel.Address.AddressLine2;
+        }
+        if (this.mergeModel.Address.Contact != selectedMergeModel.Addresses[0]?.Contact) {
             address.Contact = this.mergeModel.Address.Contact;
-        }  
-        if(this.mergeModel.Address.Country != selectedMergeModel.Addresses[0]?.Country ){
+        }
+        if (this.mergeModel.Address.Country != selectedMergeModel.Addresses[0]?.Country) {
             address.Country = this.mergeModel.Address.Country
-        }  
-        if(this.mergeModel.Address.Phone1 != selectedMergeModel.Addresses[0]?.Phone1 ){
-            address.Phone1= this.mergeModel.Address.Phone1;
-        }  
-        if(this.mergeModel.Address.Phone2 != selectedMergeModel.Addresses[0]?.Phone2 ){
+        }
+        if (this.mergeModel.Address.Phone1 != selectedMergeModel.Addresses[0]?.Phone1) {
+            address.Phone1 = this.mergeModel.Address.Phone1;
+        }
+        if (this.mergeModel.Address.Phone2 != selectedMergeModel.Addresses[0]?.Phone2) {
             address.Phone2 = this.mergeModel.Address.Phone2;
-        }  
-        if(this.mergeModel.Address.Province != selectedMergeModel.Addresses[0]?.Province ){
+        }
+        if (this.mergeModel.Address.Province != selectedMergeModel.Addresses[0]?.Province) {
             address.Province = this.mergeModel.Address.Province;
-        }  
-        if(this.mergeModel.Address.Ward != selectedMergeModel.Addresses[0]?.Ward ){
+        }
+        if (this.mergeModel.Address.Ward != selectedMergeModel.Addresses[0]?.Ward) {
             address.Ward = this.mergeModel.Address.Ward;
-        }  
-        if(address != null  && address != undefined){
+        }
+        if (address != null && address != undefined) {
             obj.Address = address;
-            obj.Address.IDPartner= obj.Id;
-            if(!selectedMergeModel.Addresses.length || !selectedMergeModel.Addresses[0].Id){
-               
+            obj.Address.IDPartner = obj.Id;
+            if (!selectedMergeModel.Addresses.length || !selectedMergeModel.Addresses[0].Id) {
+
                 obj.Address.Id = 0;
             }
-            else{
+            else {
                 obj.Address.Id = selectedMergeModel.Addresses[0].Id;
             }
-           
+
         }
         let apiPath = { method: "POST", url: function () { return ApiSetting.apiDomain("CRM/Contact/Merge") } };
-        this.pageProvider.commonService.connect(apiPath.method, apiPath.url(),obj).toPromise().then((data: any) => {
-            this.isShowMergePopup= false;
+        this.pageProvider.commonService.connect(apiPath.method, apiPath.url(), obj).toPromise().then((data: any) => {
+            this.isShowMergePopup = false;
             this.refresh();
         })
     }
-   
+
 }
