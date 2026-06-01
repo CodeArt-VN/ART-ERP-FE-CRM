8efb7705ddb7757e9e14cd48d71794ffc1b70916
Le-Hoang-Long <lehoang03long@gmail.com>
2026-06-01 10:15:49 +0700

    Approve&Disapprove

--- loyalty-policy/loyalty-policy.page.ts
+++ loyalty-policy/loyalty-policy.page.ts
@@ -4,6 +4,7 @@ import { EnvService } from 'src/app/services/core/env.service';
 import { PageBase } from 'src/app/page-base';
 import { CRM_PolLoyaltyProvider } from 'src/app/services/static/services.service';
 import { Location } from '@angular/common';
+import { ApiSetting } from 'src/app/services/static/api-setting';
 
 @Component({
 	selector: 'app-loyalty-policy',
@@ -37,5 +38,75 @@ export class LoyaltyPolicyPage extends PageBase {
 			i._Status = this.statusList.find((d) => d.Code == i.Status);
 		});
 		super.loadedData(event);
+	}
+
+	approve(): void {
+		let text = 'Duyệt';
+		let message = 'Bạn có chắc chắn duyệt các đối tượng này?';
+		this.changeStatus(text, message, 'Approved');
+	}
+
+	disapprove(): void {
+		let text = 'Không duyệt';
+		let message = 'Bạn có chắc chắn từ chối các đối tượng này?';
+		this.changeStatus(text, message, 'Unapproved');
+	}
+
+	changeStatus(text, message, Status) {
+		this.alertCtrl
+			.create({
+				header: text,
+				//subHeader: '---',
+				message: message,
+				buttons: [
+					{
+						text: 'Hủy',
+						role: 'cancel',
+						handler: () => {
+							//console.log('Không xóa');
+						},
+					},
+					{
+						text: 'Xác nhận',
+						cssClass: 'danger-btn',
+						handler: () => {
+							let publishEventCode = this.pageConfig.pageName;
+							let apiPath = {
+								method: 'POST',
+								url: function () {
+									return ApiSetting.apiDomain('CRM/PolLoyalty/ChangeStatus/');
+								},
+							};
+
+							if (this.submitAttempt == false) {
+								this.submitAttempt = true;
+								let postDTO = {
+									Ids: this.selectedItems.map((e) => e.Id),
+									Status: Status,
+								};
+								this.pageProvider.commonService
+									.connect(apiPath.method, apiPath.url(), postDTO)
+									.toPromise()
+									.then((savedItem: any) => {
+										if (publishEventCode) {
+											this.env.publishEvent({
+												Code: publishEventCode,
+											});
+										}
+										this.env.showMessage('Saving completed!', 'success');
+										this.submitAttempt = false;
+									})
+									.catch((err) => {
+										this.submitAttempt = false;
+										//console.log(err);
+									});
+							}
+						},
+					},
+				],
+			})
+			.then((alert) => {
+				alert.present();
+			});
 	}
 }
